34ba25adfa52a46e8fabe8789c79f79a4136035b
mguy02 <zelordking@gmail.com>
2017-02-19 21:38:37 +0100

    Add first try of website

--- cache/compiled/files/2b02b930213d036264724ff142b14a0b.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/opt/lampp/htdocs/DicotomixWebsite/user/config/media.yaml',
-    'modified' => 1487464703,
-    'data' => [
-        
-    ]
-];
--- cache/compiled/files/c3483e879def9c890cd981586c4fd03b.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/opt/lampp/htdocs/DicotomixWebsite/user/config/streams.yaml',
-    'modified' => 1487464703,
-    'data' => [
-        
-    ]
-];
